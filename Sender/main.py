a5517bb7f2d2d43a61d85d59a747a6d6f0302de1
Taichiro Tanaka <ta330cha@gmail.com>
2018-12-02 14:04:55 +0900

    tofコメントアウト

--- Sender/main.py
+++ Sender/main.py
@@ -11,7 +11,7 @@ import threading
 import VL53L0X #Package for Peripheral devices
 
 #Instance for Censor
-tof = VL53L0X.VL53L0X()
+# tof = VL53L0X.VL53L0X()
 
 #Settings for Censor
 MaxCensorTiming = 20000
@@ -25,14 +25,14 @@ PORT = 50007
 Interval = 1
 MaxRepeat = 10
 
-def getTiming():
-    timing = tof.get_timing()
-    if(timing < MaxCensorTiming):
-        timing = MaxCensorTiming
-    return(timing/DivCensorTiming)
+#def getTiming():
+#    timing = tof.get_timing()
+#    if(timing < MaxCensorTiming):
+#        timing = MaxCensorTiming
+#    return(timing/DivCensorTiming)
 
 def thTimer():
-    distance = tof.get_distance()
+#    distance = tof.get_distance()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOSTNAME, PORT))
         if(distance > 0):
@@ -43,12 +43,12 @@ def thTimer():
         s.sendall(temp)
         data = s.recv(1024)
         print(repr(data))
-    time.sleep(getTiming())
+#    time.sleep(getTiming())
     t = threading.Timer(Interval, thTimer)
     t.start
 
 def main():
-    tof.start_ranging(VL53L0X.VL53L0X_BEST_ACCURACY_MODE)
+#    tof.start_ranging(VL53L0X.VL53L0X_BEST_ACCURACY_MODE)
     t = threading.Timer(Interval, thTimer)
     t.start
 
